a0b550accc8cb999fe4b1fb13b6727f2cbad7187
hp <dhanashripusadkar11@gmail.com>
2022-11-04 13:08:24 +0530

    gitcommend

---
+++ src/ArrayFirst/ThirdArray.java
@@ -0,0 +1,23 @@
+package ArrayFirst;
+
+public class ThirdArray {
+    public static void main(String args[]) {
+        //Array initialiazation
+        String names[]={"vaibhav","dhansharee","dnyanehswar","aniket","pooja"};
+
+        for(String n:names)
+        {
+            System.out.println(n.toUpperCase());
+        }
+        System.out.println("total names in an arrray is :"+names.length);
+    }
+}
+
+
+
+
+
+
+
+
+
