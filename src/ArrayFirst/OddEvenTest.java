a358360df79e0844bbb81d52632bf081594b72a1
hp <dhanashripusadkar11@gmail.com>
2022-11-04 15:42:58 +0530

    gitcommend

---
+++ src/ArrayFirst/OddEvenTest.java
@@ -0,0 +1,16 @@
+package ArrayFirst;
+
+import java.util.Scanner;
+
+public class OddEvenTest {
+    public static void main(String[] args) {
+        System.out.println("enter any number");
+        int num = new Scanner(System.in).nextInt();
+        if(num%2==0)
+        System.out.println("even");
+        else
+        System.out.println("odd");
+
+
+    }
+}
