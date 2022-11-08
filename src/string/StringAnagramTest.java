08b1108d25fea47b054076f132bddb434b430735
hp <dhanashripusadkar11@gmail.com>
2022-11-08 12:30:34 +0530

    StringAnagram

---
+++ src/string/StringAnagramTest.java
@@ -0,0 +1,48 @@
+package string;
+import java.util.Arrays;
+import java.util.Scanner;
+public class StringAnagramTest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter any String");
+        String first=sc.nextLine();
+
+        System.out.println("please enter any String");
+        String second=sc.nextLine();
+
+        if(first.length()==second.length())
+        {
+            char[] f=first.toCharArray();
+            char[] s=second.toCharArray();
+
+            Arrays.sort(f);
+            Arrays.sort(s);
+
+          if (Arrays.equals(f,s))
+            {
+                System.out.println("Above strings are anagram to each other");
+            }
+          else
+            {
+                System.out.println("Above strings are not anagram to each other");
+
+            }
+            {
+                System.out.println("Above strings are anagram to each other");
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
