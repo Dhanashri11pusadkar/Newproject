0f0582d8db5323ebabbe01e1520ce7781f5a6a83
hp <dhanashripusadkar11@gmail.com>
2022-11-08 14:16:48 +0530

    palindromeTest

---
+++ src/string/StrngPalindromTest.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Scanner;
+
+public class StrngPalindromTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter any String");
+        String name = sc.nextLine();
+        String rev = "";
+        for (int i = name.length() - 1; i >= 0; i--) {
+            rev = rev + name.charAt(i);
+        }
+        System.out.println("name after reciving:" + rev);
+
+        if (rev.equals(name))
+            System.out.println("palindram string");
+        else
+            System.out.println("palindram string");
+    }
+}
+
+
