5199dd2a54d791de56a60b832395e49276919338
hp <dhanashripusadkar11@gmail.com>
2022-11-11 11:48:26 +0530

    MaxValue

---
+++ src/oops/Bike.java
@@ -0,0 +1,16 @@
+package oops;
+
+abstract class Bike {
+    abstract void run();
+}
+ class Honda extends Bike{
+     void run()
+     {
+         System.out.println("running safely");
+     }
+
+     public static void main(String[] args) {
+         Bike b1=new Honda();
+         b1.run();
+     }
+     }
