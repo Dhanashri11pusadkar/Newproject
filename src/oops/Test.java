48722cbcb74855c6bf2fec51f00c6b759e13844d
hp <dhanashripusadkar11@gmail.com>
2022-11-16 15:13:19 +0530

    interface

---
+++ src/oops/Test.java
@@ -0,0 +1,13 @@
+package oops;
+
+ class Test {
+     public static void main(String[] args) {
+         new Thread(new Runnable() {
+             @Override
+             public void run() {
+                 System.out.println("New thread created");
+             }
+         }).start();
+     }
+}
+
