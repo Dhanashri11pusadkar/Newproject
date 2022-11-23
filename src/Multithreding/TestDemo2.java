a46dc52da2572a58411a0f992a7f0710f2372e6a
hp <dhanashripusadkar11@gmail.com>
2022-11-23 14:46:45 +0530

    Multhreding

---
+++ src/Multithreding/TestDemo2.java
@@ -0,0 +1,46 @@
+package Multithreding;
+class Java extends Thread
+{
+    public void run()
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("Java");
+            try { Thread.sleep(1000);  } catch (InterruptedException e) { throw new RuntimeException(e);}
+        }
+    }
+}
+class Android extends Thread
+{
+    public void run()
+    {
+        printTenTimes();
+    }
+    private void printTenTimes() {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("Android");
+            try { Thread.sleep(1000);  } catch (InterruptedException e) { throw new RuntimeException(e);}
+        }
+    }
+}
+public class TestDemo2 {
+    public static void main(String[] args) {
+        class ThreadDemo2 {
+            public static void main(String[] args) {
+                Thread thread1 = new Java();//upcasting.
+                Thread thread2 = new Android();//upcasting.
+
+                thread1.start();//to start the thread
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                thread2.start();//start method internally calls run() method*/
+
+            }
+
+        }
+    }
+}
